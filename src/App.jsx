b116f67a56b61ae8f0b1febab6028642b5e8456a
luckyBravine <bravinlude@gmail.com>
2023-09-02 14:16:41 +0300

    commencing on implementation of Cool Housing slider

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <div className="bg-slate-100 w-full h-full overflow-hidden">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
